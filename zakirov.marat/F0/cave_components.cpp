74041cb390442c433edeaf11b324a005ad315134
ZakirovMS <mz.08.08.06@gmail.com>
2025-06-24 20:55:55 +0300

    operator >> and << implementation

--- zakirov.marat/F0/cave_components.cpp
+++ zakirov.marat/F0/cave_components.cpp
@@ -1,1 +1,90 @@
+#include "cave_components.hpp"
+#include <iostream>
+#include <stream_guardian.hpp>
 
+namespace
+{
+  struct MinorSymbol
+  {
+    char symbol;
+  };
+
+  std::istream & operator>>(std::istream & in, MinorSymbol && sym)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+
+    char symbol;
+    if (!(in >> symbol) || symbol != sym.symbol)
+    {
+      in.setstate(std::ios::failbit);
+    }
+
+    return in;
+  }
+}
+
+bool zakirov::CaveComponent::isWall() const noexcept
+{
+  return cell_ == '#';
+}
+
+bool zakirov::CaveComponent::isPath() const noexcept
+{
+  return cell_ == '.';
+}
+
+bool zakirov::CaveComponent::isEntry() const noexcept
+{
+  return cell_ == '+';
+}
+
+bool zakirov::CaveComponent::isExit() const noexcept
+{
+ return (cell_ == '=' || (cell_ > 'a' && cell_ < 'z') || (cell_ > 'A' && cell_ < 'Z'));
+}
+
+std::istream & zakirov::operator>>(std::istream & in, zakirov::CaveComponent & comp_c)
+{
+  char element_c;
+  in >> element_c;
+  comp_c.setCell(element_c);
+  if (!comp_c.isWall() && !comp_c.isPath() && !comp_c.isEntry() && !comp_c.isExit())
+  {
+    in.setstate(std::ios::failbit);
+  }
+
+  return in;
+}
+
+std::istream & zakirov::operator>>(std::istream & in, zakirov::CaveLayer & layer_c)
+{
+  Guardian guard(in);
+  char name;
+  int width = 0;
+  int length = 0;
+  in >> std::noskipws;
+  in >> name >> MinorSymbol{' '};
+  in >> length >> MinorSymbol{' '} >> width >> MinorSymbol{'\n'};
+  layer_c.setName(name);
+  layer_c.setCords({length, width});
+  size_t key = 0;
+  CaveComponent inserter;
+  for (size_t i = 0; i < width; ++i)
+  {
+    in >> inserter;
+    layer_c.addElement(key, inserter);
+    for (size_t j = 0; j < length - 1; ++j)
+    {
+      in >> MinorSymbol{' '} >> inserter;
+      layer_c.addElement(key, inserter);
+    }
+
+    in >> MinorSymbol{'\n'};
+  }
+
+  return in;
+}
--- zakirov.marat/F0/cave_components.hpp
+++ zakirov.marat/F0/cave_components.hpp
@@ -2,29 +2,41 @@
 #define CAVE_COMPONENTS_HPP
 #include <map>
 #include <iostream>
-
+#include <string>
 namespace zakirov
 {
-  class Cave
-  {
-  public:
-    Cave(std::istream & in);
-    Cave getPath();
-    void addEdge(int u, char v);
-  private:
-    std::map< int, std::map< int, CaveComponent > > cave_map;
-  };
-
   class CaveComponent
   {
     public:
-      bool isWall();
-      bool isPath();
-      bool isEntry();
-      bool isExit();
+      char getCell() const noexcept;
+      char setCell(char cell) noexcept;
+      bool isWall() const noexcept;
+      bool isPath() const noexcept;
+      bool isEntry() const noexcept;
+      bool isExit() const noexcept;
     private:
-      char symbol;
+      char cell_;
   };
+
+  class CaveLayer
+  {
+  public:
+    CaveLayer(std::istream & in);
+    CaveLayer getPath();
+    std::pair< int, int > getCords() const noexcept;
+    void setCords(std::pair< int, int > cords) noexcept;
+    char getName() const noexcept;
+    void setName(char name) noexcept;
+    void addElement(int key, CaveComponent cell);
+    void addEdge(int u, char v);
+  private:
+    std::map< int, std::map< int, CaveComponent > > cave_map_;
+    char name_;
+    std::pair< int, int > cords_;
+  };
+
+  std::istream & operator>>(std::istream & in, CaveComponent & comp_c);
+  std::istream & operator>>(std::istream & in, CaveLayer & layer_c);
 }
 
 #endif
